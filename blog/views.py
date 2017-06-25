344f173d8b38b44e03d7226ccf65b7cf4b123e07
root <root@localhost.localdomain>
2017-06-25 07:15:37 +0700

    make edit page, hiding create&edit button

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -9,6 +9,35 @@ def post_list(request):
 	return render(request, 'blog/post_list.html', {'posts':posts})
 
 
-def post_detail(request, post_pk):
-	post = Post.objects.get(pk=post_pk)
+def post_detail(request, pk):
+	post = Post.objects.get(pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_create(request):
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('blog:post_detail', post.pk)
+			#redirect must contain url namespace wich is 'blog:post_detail'
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_create.html', {'form':form})
+
+
+def post_edit(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == 'POST':
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('blog:post_detail', post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form':form})
