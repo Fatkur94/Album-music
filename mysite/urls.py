b99dd392015dba4a77ee6241d1ae9fb3b7d3316d
root <root@localhost.localdomain>
2017-06-27 12:34:26 +0700

    adding draft & login/logout required

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,24 +17,29 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views
 from music import urls as music_urls
 from blog import urls as blog_urls
-from . import views
+from . import views as view
+#accesing login django
 
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^path/$', views.index),
-    url(r'^user/$', views.display_good2),
-    url(r'^meta/$', views.display_meta),
-    url(r'^time/$', views.current_datetime),
-    url(r'^time/plus/(\d{1,2})/$', views.hours_ahead),
-    url(r'^contact/$', views.contact),
-    url(r'^listbook/$', views.display_book),
-    url(r'^publisher/$', views.PublisherList.as_view()),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^path/$', view.index),
+    url(r'^user/$', view.display_good2),
+    url(r'^meta/$', view.display_meta),
+    url(r'^time/$', view.current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', view.hours_ahead),
+    url(r'^contact/$', view.contact),
+    url(r'^listbook/$', view.display_book),
+    url(r'^publisher/$', view.PublisherList.as_view()),
     #dont include $  in /music if not adding url again
     #like /music/list etc
     url(r'^music/', include(music_urls, namespace='music')),
-    url(r'^blog/', include(blog_urls, namespace='blog')),
+    url(r'', include(blog_urls, namespace='blog')),
+    
 ]
